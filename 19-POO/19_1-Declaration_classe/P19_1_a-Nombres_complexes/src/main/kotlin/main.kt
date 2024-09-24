05b54734846ac644f81295c4a7e11bd72238223f
MatthisYA <matthisya77@gmail.com>
2024-09-24 11:49:27 +0200

    24.09.2024

--- 19-POO/19_1-Declaration_classe/P19_1_a-Nombres_complexes/src/main/kotlin/main.kt
+++ 19-POO/19_1-Declaration_classe/P19_1_a-Nombres_complexes/src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
-// write the class Complex here
+class Complex(var reel:Double=0.0, var image:Double=0.0)
 
 
 fun main() {
     val number1 = Complex()
-    number1.real = 5.0
+    number1.reel = 5.0
     number1.image = 2.0
-    println(number1.real)
+    println(number1.reel)
     println(number1.image)
 }
--- 19-POO/19_1-Declaration_classe/P19_1_b-Planete/src/main/kotlin/main.kt
+++ 19-POO/19_1-Declaration_classe/P19_1_b-Planete/src/main/kotlin/main.kt
@@ -1,9 +1,13 @@
-// write your code here
+class Planet(){
+    var Star:String="Unknow Star"
+    var Mass:Double=1.0
+    var numberOfSatellites:Int=1
+}
 
 
 fun main() {
     val kotlinPlanet = Planet()
-    kotlinPlanet.star = "Java"
-    kotlinPlanet.mass = 2.0
+    kotlinPlanet.Star = "Java"
+    kotlinPlanet.Mass = 2.0
     kotlinPlanet.numberOfSatellites = 0
 }
